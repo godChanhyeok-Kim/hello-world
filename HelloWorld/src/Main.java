6e26a666c3144e77023871307524f3726544aaa4
chanstars <c3370753@naver.com>
2023-09-01 17:11:09 +0900

    HelloWorld 파일 올리기

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        System.out.println("h");
+        System.out.println("e");
+        System.out.println("l");
+        System.out.println("l");
+        System.out.println("o");
+
+        ArrayList<String> arr = new ArrayList<>();
+
+        arr.add("Hello");
+        arr.add("world");
+
+        System.out.println(arr);
+
+    }
+}
